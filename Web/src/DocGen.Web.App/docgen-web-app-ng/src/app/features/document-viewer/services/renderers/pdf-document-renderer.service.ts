681b614fa8a8b12dd6902168a8b5c7bb60e8fef7
Michael Fry <michaelfry2002@gmail.com>
2018-03-12 08:53:35 +1300

    Fix typings of pdfkit

--- Web/src/DocGen.Web.App/docgen-web-app-ng/src/app/features/document-viewer/services/renderers/pdf-document-renderer.service.ts
+++ Web/src/DocGen.Web.App/docgen-web-app-ng/src/app/features/document-viewer/services/renderers/pdf-document-renderer.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+
 import * as PDFDocument from '../../../../../thirdparty/pdfkit';
 import * as blobStream from '../../../../../thirdparty/blob-stream';
+import * as PDFKit from 'pdfkit';
 
 import { SerializableDocument } from '../../../core';
 
@@ -12,7 +14,7 @@ export class PdfDocumentRendererService {
   constructor() { }
 
   render(document: SerializableDocument): Promise<string> {
-    const pdfDoc = new PDFDocument();
+    const pdfDoc: PDFKit.PDFDocument = new PDFDocument();
 
     const stream = pdfDoc.pipe(blobStream());
     pdfDoc.text('Hello world!');
@@ -24,7 +26,5 @@ export class PdfDocumentRendererService {
         resolve(stream.toBlobURL('application/pdf'));
       });
     });
-
-    // return new Promise(resolve => resolve(''));
   }
 }
